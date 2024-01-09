72d319aef0099548dd442aa8ef206f11e0fd975e
bhagirathauti <bhagirath961778@gmail.com>
2024-01-09 10:53:05 +0530

    done

---
+++ pronote/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Editor from './Components/Editor'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <Editor />
+      </div>
+    </>
+  )
+}
+
+export default App
